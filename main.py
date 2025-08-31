cd49dfcc8ca52f44dab74e6a05d04a9cace2d252
faisal.almatar <faisal.almatar973@gmail.com>
2025-08-31 16:08:01 +0300

    added missing auth

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    # Later, add your deployed frontend origin, e.g.:
-    # "https://your-frontend.example.com"
+    # Later, add the deployed frontend origin:
+    
 ]
 
 app.add_middleware(
@@ -28,8 +28,7 @@ app.add_middleware(
     allow_origins=origins,     # Which sites can call this API
     allow_methods=["*"],       # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],       # Allow all headers (e.g., Content-Type, Authorization)
-    # NOTE: We are NOT using credentials in this simple lesson,
-    # so we are not setting allow_credentials.
+    allow_credentials=True,
 )
 
 
