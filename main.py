95bca9211cb8f57ece0fa6235ef5b1ca9f320758
“AM-973” <AlMatar973@gmail.com>
2025-08-31 16:10:48 +0300

    added cors

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from controllers.expenses import router as ExpensesRouter
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "http://localhost:5173",
     "http://127.0.0.1:3000",
     # Later, add the deployed frontend origin:
     
