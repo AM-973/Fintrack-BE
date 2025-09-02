9574b505a58f3c9002d4df1f40c7cd9c75f7dba6
“AM-973” <AlMatar973@gmail.com>
2025-09-03 00:46:02 +0300

    heroku validation

--- config/environment.py
+++ config/environment.py
@@ -1,5 +1,11 @@
 import os
 
+if os.getenv("APP_ENV", "development") != "production":
+    # load .env only locally
+    from dotenv import load_dotenv
+    load_dotenv()
+
+DATABASE_URL = os.getenv("DB_URI")
 db_URI = os.getenv("DB_URI")
 secret = os.getenv("secret")
 
