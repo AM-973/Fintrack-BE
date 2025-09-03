9b000914efc3a1f654c98694092aa0aa7dea1420
“AM-973” <AlMatar973@gmail.com>
2025-09-04 01:29:26 +0300

    heroku

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://fintrack-p3f15hk6v-almatar973-gmailcoms-projects.vercel.app",
-    "https://fintrack-qqevdfx78-almatar973-gmailcoms-projects.vercel.app/"
+    "fintrack-fe-git-main-almatar973-gmailcoms-projects.vercel.app"
     
 ]
 
